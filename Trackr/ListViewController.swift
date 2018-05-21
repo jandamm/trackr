b29e23635633fdeffa9db88b926bf64d0028bced
Jan Dammshäuser <jandammshaeuser@gmail.com>
2018-05-21 12:46:52 +0200

    Add basic display

---
+++ Trackr/ListViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Trackr
+//
+//  Created by Jan Dammshäuser on 21.05.18.
+//  Copyright © 2018 Jan Dammshäuser. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+	let data: [String] = ["arst"]
+}
+
+extension ListViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return data.count
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "List.Cell", for: indexPath)
+		cell.textLabel?.text = data[indexPath.row]
+		return cell
+	}
+}
+
+extension ListViewController: UITableViewDelegate {
+}
